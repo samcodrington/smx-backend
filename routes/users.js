ced07ae453b0acfc484e834d92a6efcd7bfb5457
Sam Codrington <sam.codrington@queensu.ca>
2017-10-27 16:18:31 -0400

    complete routing with console logging comments for GET and POST

--- routes/users.js
+++ routes/users.js
@@ -7,21 +7,26 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //All routing methods are mounted on /users/
 router.all('*',function(req,res, next){
-  console.log("You've reached our user");
+  //Logging comments
+  console.log("Request made for " + req.baseURL);
+  console.log("Body is as follows");
   console.log(req.body);
   next();
 })
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log("Get Request");
   res.send('respond with a resource');
   next();
 });
+
 /* POST users listing. */
 router.post('/', urlencodedParser, function(req,res,next){
-  res.send('Successfully signed up user');
-  
+  console.log("Post Request");  
   addUser(req.body);
   // need error handling // TODO
+  res.send('Successfully signed up user');
   next();
 });
 
