d0c882c51720c4d8f41a06f252ce7cc79da38904
13mtfb <burton.matthew@hotmail.com>
2017-10-28 13:13:37 -0400

    resolve #3

--- controllers/addUser.js
+++ controllers/addUser.js
@@ -4,20 +4,18 @@ const User = require("../models/user");
 
 var addUser = function(FrontendUser){
 //Parse frontend input into user schema
-var newUser = new User({
-  username: FrontendUser.username,
-  password: FrontendUser.password,
-  nameFirst: FrontendUser.nameFirst,
-  nameLast: FrontendUser.nameLast,
-  email: FrontendUser.email,
-  school: FrontendUser.school,
-});
+  var newUser = new User({
+    username: FrontendUser.username,
+    password: FrontendUser.password,
+    nameFirst: FrontendUser.nameFirst,
+    nameLast: FrontendUser.nameLast,
+    email: FrontendUser.email,
+    school: FrontendUser.school,
+  });
 
-newUser.save().then(function(){//create a promise on this method
-  console.log("saved user to DB");
-}).on("error",function(error){
-  console.log("add user error");
-});//asynchronous process
+  newUser.save().then(function(resolve,reject){
+    if (resolve){ console.log("save succesfull");}
+  });
 
 };
 
