920668bc7e7daa00eb6f13094c830a0b977ee002
Sam Codrington <sam.codrington@queensu.ca>
2017-11-18 16:50:10 -0500

    fix serialize/deserialize

--- config/passport.js
+++ config/passport.js
@@ -17,10 +17,10 @@ passport.use('login', new LocalStrategy({
                 return done(err); }
             if (!user) {
                 console.log('error! can\'t find user');
-                return done(null, false);}
+                return done(err, false);}
             if (user.password != password){
                 console.log('error! wrong password');                
-                return done(null, false);
+                return done(err, false);
             }
             console.log('found user');
             return done(null, user);
@@ -28,10 +28,13 @@ passport.use('login', new LocalStrategy({
     }
 ));
 
-passport.serializeUser(function(user,cb){
-    cb(null, user.id);
+passport.serializeUser(function(user,done){
+    console.log('Serializing User: ' + user);
+    done(null, user.id);
 });
 
 passport.deserializeUser(function(id, cb){
-   // db.User.
+    db.User.findByID(id).then((user) => {
+        done(null, user.id);
+    })
 });
