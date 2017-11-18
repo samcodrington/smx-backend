0c22b717045924df1150b82818d8110b8767ddfd
Sam Codrington <sam.codrington@queensu.ca>
2017-11-18 14:05:21 -0500

    initial routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var auth = require('./routes/auth');
 
 //require database
 var db = require('./controllers/connection.js');
