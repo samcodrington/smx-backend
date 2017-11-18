5d462c4295e0480ed71071e895c9fdff7ac1f613
Sam Codrington <sam.codrington@queensu.ca>
2017-11-18 17:26:16 -0500

    add on to login/logout but can't receive cookies yet

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,18 @@ const passport = require('passport');
 router.post('/login', passport.authenticate('login'),
     function(req,res){
         console.log('accessing login');
-        console.log('got user: '+ req.user);
+        res.send(200, req.user);
     //res.render('logged in'); 
     //^--throws error due to lack of views engine
 });
 
 router.post('/logout', function(req,res) {
-    req.logout();
+    if (req.user){
+        req.logout();
+        res.send(200, 'logged out');
+    }
+    else res.send(404, 'not logged in!');
+        
     //res.send('logged out');
 });
 module.exports = router;
