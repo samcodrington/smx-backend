b38e50b22b13548b536cffd03b1078acbc2ea9f6
Sam Codrington <sam.codrington@queensu.ca>
2017-11-18 16:32:50 -0500

    add routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
+const passport = require('passport');
 
-router.post('/login', (req,res) => {
-    console.log('accessing login');
-    
+router.post('/login', passport.authenticate('login'),
+    function(req,res){
+        console.log('accessing login');
+        console.log('got user: '+ req.user);
     //res.render('logged in'); 
     //^--throws error due to lack of views engine
 });
 
-router.post('/logout', (req,res) => {
+router.post('/logout', function(req,res) {
+    req.logout();
     //res.send('logged out');
 });
 module.exports = router;
