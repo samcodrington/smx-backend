a13f69bbc8cdef3771f8687b7ce6c132403367e4
Sam Codrington <sam.codrington@queensu.ca>
2017-11-20 16:37:18 -0500

    finalize views with commented code to indicate where cookies are stored

--- routes/auth.js
+++ routes/auth.js
@@ -6,21 +6,18 @@ router.post('/login', passport.authenticate('login'),
         console.log('accessing login');
         //console.log(res.req.headers.cookie); //this is where cookie is returned.
         res.status(200).send('Logged in');
-    //res.render('logged in'); 
-    //^--throws error due to lack of views engine
 });
 
 router.post('/logout', function(req,res) {
-    console.log("beginning logout" + req.session);
+    console.log("beginning logout");
+    //console.log(req.headers.cookie); //this is where cookie is checked!!
     if (req.user){
-        console.log("User is logged in");
+        console.log("User is logged out");
         req.logout();
         req.session.destroy(function(err){
             res.status(200).send('logged out');
         });
     }
     else res.status(404).send('not logged in!');
-        
-    //res.send('logged out');
 });
 module.exports = router;
