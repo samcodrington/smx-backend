fedbe151346a88a150b8a1db79b7525a37ab6a09
Sam Codrington <sam.codrington@queensu.ca>
2017-12-23 15:26:54 -0500

    added CORS headers to all responses

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.use(session({ secret: 'blahblah', cookie: { maxAge: 5*60*1000} })); // maxAg
 // initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use('*', function(req,res,next){
