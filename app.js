3a7ac00183a15c4488985f0f7c291d404ce4162c
Sam Codrington <sam.codrington@queensu.ca>
2018-01-08 14:56:16 -0500

    add additional preflight headers

--- app.js
+++ app.js
@@ -33,7 +33,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, OPTIONS")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
