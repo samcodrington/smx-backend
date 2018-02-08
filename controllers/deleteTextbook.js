1be25d6a849bd7a484929d69724a20395969fb6b
Sam Codrington <sam.codrington@queensu.ca>
2018-02-08 10:00:28 -0500

    create deleteTextbook, start delTextFromUser

---
+++ controllers/deleteTextbook.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Textbook = require("../models/textbookSchema");
+const User = require("../models/userSchema");
+
+exports.deleteTextbook = function(textbookID){
+    Textbook.findByIdAndRemove(textbookID)
+    .then(function (resolve) {
+        if (err) return handleError(err);
+        // removed!
+    });
+}
+exports.deleteTextbookFromUser = function(textbookID, UserID){
+    User.findById(UserID, function(err, u){
+        if (err) return "unable to find User"
+        if (u.postedtextbooks.includes({"id": textbookID})){
+            u.postedtextbooks.delete({"id": textbookID})
+        }
+    });
+    
+}
