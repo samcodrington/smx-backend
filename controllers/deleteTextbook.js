e38cacc6074e93e9abb84f44eb714c0efd2ac945
13mtfb <burton.matthew@hotmail.com>
2018-03-20 23:08:00 -0400

    Fix deleteTextbook method

--- controllers/deleteTextbook.js
+++ controllers/deleteTextbook.js
@@ -3,10 +3,9 @@ const Textbook = require("../models/textbookSchema");
 const User = require("../models/userSchema");
 
 exports.deleteTextbook = function(textbookID){
-    return new Promise(function(resolve, reject){
-        Textbook.findByIdAndRemove(textbookID)
-        .then(function(t){ resolve("Deleted Textbook #" + t);}) 
-        .catch(function (t) {reject("Unable to Delete Textbook #" + t)});
+    return Textbook.findByIdAndRemove(textbookID).then(function(resolve){
+      if (!resolve)//textbook not found in database
+        return Promise.reject("Textbook cannot be found in database");
     });
 }
 
@@ -23,18 +22,18 @@ function userContainsTBook (user, tid){
 exports.deleteTextbookFromUser = function(UserID, textbookID){
     return new Promise(function(resolve, reject){
         User.findById(UserID)
-        .then(function (u){ 
+        .then(function (u){
             if (!isEmpty(u)){
                 //get indice of textbook in postedTextbooks
-                var ind = userContainsTBook(u, textbookID);                
+                var ind = userContainsTBook(u, textbookID);
                 //if textbook not attached to user
                 if (ind == -1){
-                    
+
                     return reject("Textbook not associated with user.\nAssociated IDs are: \n"+ u.postedtextbooks);
 
                 } else {
-                    
-                    u.postedtextbooks.splice(ind);                    
+
+                    u.postedtextbooks.splice(ind);
                     u.save(function(err, newU){
                         if (err) return reject("couldn't update User");
                         //if User is updated, remove textbook from Textbook collection
@@ -43,7 +42,7 @@ exports.deleteTextbookFromUser = function(UserID, textbookID){
                             .then(()=>{ return resolve(newU);})
                             .catch(()=> {return reject(err);});
                         }
-                    });                
+                    });
                 }
             }
         })
